31fafd36720a294cb92ee75bca054c199072dc7f
Adia-wty <39758262+Adia-wty@users.noreply.github.com>
2019-08-07 23:21:13 +0800

    Add files via upload

---
+++ Assets/Scripts/BlockSpawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSpawner : MonoBehaviour {
+
+    public Transform[] spawnPoints;
+
+    public GameObject blockPrefab;
+
+    public float timeBetweenWaves = 1f;
+
+    private float timeToSpwan = 2f;
+
+    // Use this for initialization
+    void Update()
+    {
+        if (Time.time >= timeToSpwan)
+        {
+            SpwanBlocks();
+            timeToSpwan = Time.time + timeBetweenWaves;
+        }
+
+    }
+
+    void SpwanBlocks()
+    {
+        int randomIndex = Random.Range(0, spawnPoints.Length);
+
+        for (int i = 0; i < spawnPoints.Length; i++)
+        {
+            if (randomIndex != i)
+            {
+                Instantiate(blockPrefab, spawnPoints[i].position, Quaternion.identity);
+            }
+
+        }   
+    }
+}
